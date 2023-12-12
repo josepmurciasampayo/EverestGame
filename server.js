91fef410c5665b2fcc5a3a0fe09b27d22fbb299a
Joseph <josepmurciasampayo@gmail.com>
2023-12-12 09:32:45 -0500

    Add backend

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+app.use(express.static(__dirname + '/public'));
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+server.listen(8081, function () {
+  console.log(`Listening on ${server.address().port}`);
+});
+
+io.on('connection', function (socket) {
+    console.log(socket.id + '  : user connected');
+    socket.on('disconnect', function () {
+      console.log('user disconnected');
+    });
+});
